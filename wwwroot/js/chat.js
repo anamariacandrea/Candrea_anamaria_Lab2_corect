d123433f9f2f535fd886d5437f38ae037ddaf314
Ana-Maria Candrea <candrea_anamaria@yahoo.com>
2020-12-03 22:02:26 +0200

    Laborator 9 Laborator 8+task-urile din lab-ul 9

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -37,8 +37,9 @@ document.getElementById("sendButton").addEventListener("click", function (event)
         return console.error(err.toString());
     });
     document.getElementById("sendButton").addEventListener("click", function (event) {
-        var user = document.getElementById("userInput").value;
         var message = document.getElementById("messageInput").value;
-        connection.invoke("SendMessage", user, message).catch(function (err) {
+        connection.invoke("SendMessage", "", message).catch(function (err) {
             return console.error(err.toString());
         });
+        event.preventDefault();
+    
